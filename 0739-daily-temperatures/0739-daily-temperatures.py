6f90ac9062df83aef67dee4d5b4b944b510b5ab8
Minuk Cho <154600451+ADALIV@users.noreply.github.com>
2025-02-17 16:40:36 +0900

    Time: 161 ms (71.68%), Space: 24.5 MB (94.06%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,19 @@
+from collections import deque
+
 class Solution(object):
     def dailyTemperatures(self, temperatures):
         """
         :type temperatures: List[int]
         :rtype: List[int]
         """
+        dq = deque()
         n = len(temperatures)
-        answer = [0] * n  # 결과 배열 (기본값 0)
-        stack = []  # 인덱스를 저장할 스택 (단조 감소 스택)
+        i = 0
+        answer = [0]*n
 
         for i, temp in enumerate(temperatures):
-            # 현재 온도가 스택 top의 온도보다 높다면 정답 갱신
-            while stack and temperatures[stack[-1]] < temp:
-                prev_index = stack.pop()  # 이전 인덱스
-                answer[prev_index] = i - prev_index  # 현재 인덱스 - 이전 인덱스
-            stack.append(i)  # 현재 인덱스 추가
-    
+            while dq and temperatures[dq[-1]]<temp:
+                index = dq.pop()
+                answer[index] = i - index
+            dq.append(i)
         return answer
