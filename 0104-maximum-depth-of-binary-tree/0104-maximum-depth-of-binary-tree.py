25a903001e19f9056eaa4b4a7275caa335c2d547
Minuk Cho <154600451+ADALIV@users.noreply.github.com>
2025-02-21 13:04:24 +0900

    Time: 1 ms (76.07%), Space: 15.1 MB (43.35%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,24 +5,25 @@
 #         self.left = left
 #         self.right = right
 
+from collections import deque
+
 class Solution(object):
     def maxDepth(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        max_depth = self.DFS(root, 1)
-        return max_depth
-    def DFS(self, root, cnt):
         if root is None:
-            return cnt - 1
-        
-        # 왼쪽과 오른쪽 서브트리의 깊이를 초기화
-        lv = rv = cnt
-        
-        if root.left:
-            lv = self.DFS(root.left, cnt + 1)  # 왼쪽 서브트리 깊이 계산
-        if root.right:
-            rv = self.DFS(root.right, cnt + 1)  # 오른쪽 서브트리 깊이 계산
+            return 0
+        max_v = 0
+        dq = deque()
+        dq.append((root, 1))
 
-        return max(lv, rv)
+        while dq:
+            node, depth = dq.popleft()
+            max_v = max(max_v, depth)
+            if node.left:
+                dq.append((node.left, depth+1))
+            if node.right:
+                dq.append((node.right, depth+1))
+        return max_v
