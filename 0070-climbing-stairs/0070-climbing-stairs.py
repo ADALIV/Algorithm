420084bd00d95203ebc874ad48b993988a99ea3f
Minuk Cho <154600451+ADALIV@users.noreply.github.com>
2025-05-25 09:59:05 +0900

    Time: 0 ms (100%), Space: 12.5 MB (52.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 1:
-            return 1
-        elif n == 2:
-            return 2
+        if n <= 2:
+            return n
         
-        # 재귀적으로 계산
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        a, b = 1, 2
+        for i in range(3, n+1):
+            a, b = b, a + b
+        return b
